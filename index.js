c80323d429f87a50f217bd3310f1c472c375837e
dalia sawaya <dalia.sawaya@flatironschool.com>
2020-03-06 15:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function wakeDog(a, b) {
-  console.log(`Wake ${a} the poodle`)
+  console.log(`Wake ${a} the ${b}`)
 }
 
 function leashDog(a, b) {
-  console.log(`Leash ${a} the poodle`)
+  console.log(`Leash ${a} the ${b}`)
 }
 
 function walkToPark(a, b) {
-  console.log(`Walk to the park with ${a} the poodle`)
+  console.log(`Walk to the park with ${a} the ${b}`)
 }
 
 function throwFrisbee(a, b) {
-  console.log(`Throw the frisbee for ${a} the poodle`)
+  console.log(`Throw the frisbee for ${a} the ${b}`)
 }
 
 function walkHome(a, b) {
-  console.log(`Walk home with ${a} the poodle`)
+  console.log(`Walk home with ${a} the ${b}`)
 }
 
 function unleashDog(a, b) {
-  console.log(`Unleash ${a} the poodle`)
+  console.log(`Unleash ${a} the ${b}`)
 }
 
 function exerciseDog(dogName, dogBreed) {
