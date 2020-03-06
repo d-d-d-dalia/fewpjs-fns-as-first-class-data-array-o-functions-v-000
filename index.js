4f0ddc7f5f1aeb15fd16bfab734c939d5e0dd678
dalia sawaya <dalia.sawaya@flatironschool.com>
2020-03-06 15:42:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function unleashDog(a, b) {
 }
 
 function exerciseDog(dogName, dogBreed) {
-  routine.forEach(func => func(dogName, dogBreed))
+  routine.map(func => func(dogName, dogBreed))
 }
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
